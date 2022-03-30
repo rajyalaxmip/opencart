9a4c4b3ed8c8fb72c2c767e3dff00fad7a4dee68
puli <p.rajyalaxmi@gmail.com>
2022-03-30 12:43:37 -0500

    added to Account Page

--- src/test/java/pageObjects/AccountPage.java
+++ src/test/java/pageObjects/AccountPage.java
@@ -1,5 +1,16 @@
 package pageObjects;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class AccountPage {
+	
+	public  WebDriver driver;
+	
+	@FindBy(name="email")
+	WebElement txtEmail;
+
+	
 
 }
